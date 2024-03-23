a787a0e6a993bc9366ae9ae3c39789e3ac52b957
site.net <129742936+Armin3080@users.noreply.github.com>
2024-03-23 20:51:51 +0330

    Add files via upload

---
+++ scriptindex.js
@@ -0,0 +1,12 @@
+function checkLogin(event) {
+    event.preventDefault();
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+  
+    // بررسی یوزرنیم و رمز عبور
+    if (username === "venteda" && password === "DCKPE") {
+      window.location.href = 'index2.html'; // مسیر صفحه مقصد را وارد کنید
+    } else {
+      alert('Username or password is wrong!');
+    }
+  }
